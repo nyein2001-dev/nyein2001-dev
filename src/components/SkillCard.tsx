cf0dd1eae20a5659ed4831631c8923092a11a2a7
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-23 05:24:38 +0630

    badge decoration

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -8,12 +8,14 @@ const SkillsCard: React.FC = () => {
 
   return (
     <div className="rounded-xl bg-white p-7 dark:bg-night-800">
-      <h2 className="mb-4 text-lg font-semibold dark:text-night-50">{t("skills")}</h2>
+      <h2 className="mb-4 text-lg font-semibold dark:text-night-50">
+        {t("skills")}
+      </h2>
       <div className="-m-2 flex flex-wrap">
         {resume.skill.map((value, index) => (
           <span
             key={index}
-            className="rounded-md font-medium text-sm px-2 py-0.5 badge m-1 cursor-pointer bg-emerald-500/20 text-emerald-500 hover:bg-emerald-500 hover:text-white"
+            className="inline-flex rounded-lg px-2 py-1 text-xs font-medium m-1 cursor-pointer bg-emerald-500/20 text-emerald-500 hover:bg-emerald-500 hover:text-white"
           >
             {value}
           </span>
