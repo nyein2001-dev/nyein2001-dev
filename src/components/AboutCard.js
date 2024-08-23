b4f7113eada8b2660fc441b24b88b43f9c4266cb
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-23 20:24:10 +0630

    enhance menu ui

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -14,26 +14,33 @@ const AboutCard = () => {
   ];
 
   return (
-    <div className="rounded-xl bg-white p-7 dark:bg-night-800">
-      <h2 className="mb-4 text-lg font-semibold dark:text-night-50">
-        {i18n.t("about")}
-      </h2>
-      <p className="mb-5 text-sm text-gray-600 dark:text-night-200">
-        {resume.profile.aboutMe}
-      </p>
-      <ul className="inline-flex space-x-2 font-semibold">
-        {menu.map((item, index) => (
-          <li key={index}>
-            <Link
-              to={item.route}
-              className="inline-flex cursor-pointer items-center gap-1 rounded-lg border-2 border-gray-100 px-2 py-1.5 text-xs font-medium text-gray-400 hover:border-primary-500 hover:bg-primary-500 hover:text-white dark:border-night-700 dark:text-night-400 dark:hover:border-primary-500 dark:hover:bg-primary-500/20 dark:hover:text-primary-500"
-            >
-              <HeroIcon icon={item.icon} className="hidden h-4 w-4 sm:block" />
-              <span>{i18n.t(item.name)}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <div className="rounded-xl bg-white mb-5 p-7 dark:bg-night-800">
+        <h2 className="mb-4 text-lg font-semibold dark:text-night-50">
+          {i18n.t("about")}
+        </h2>
+        <p className="mb-5 text-sm text-gray-600 dark:text-night-200">
+          {resume.profile.aboutMe}
+        </p>
+      </div>
+      <div className="rounded-xl bg-white p-3 dark:bg-night-800">
+        <ul className="inline-flex space-x-2 font-semibold">
+          {menu.map((item, index) => (
+            <li key={index}>
+              <Link
+                to={item.route}
+                className="inline-flex cursor-pointer items-center gap-1 rounded-lg border-2 border-gray-100 px-2 py-1.5 text-xs font-medium text-gray-400 hover:border-primary-500 hover:bg-primary-500 hover:text-white dark:border-night-700 dark:text-night-400 dark:hover:border-primary-500 dark:hover:bg-primary-500/20 dark:hover:text-primary-500"
+              >
+                <HeroIcon
+                  icon={item.icon}
+                  className="hidden h-4 w-4 sm:block"
+                />
+                <span>{i18n.t(item.name)}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
