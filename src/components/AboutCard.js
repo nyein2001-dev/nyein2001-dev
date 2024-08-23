33f1ee84a15255518df958642ea3ef986976b0ea
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-23 20:55:28 +0630

    remember route location

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import HeroIcon from "./HeroIcon.tsx";
 import { useResumeStore } from "../stores/ResumeContext";
 import i18n from "../i18n";
 
 const AboutCard = () => {
   const { resume } = useResumeStore();
+  const location = useLocation();
 
   const menu = [
     { route: "/", name: "menu.experience", icon: "BriefcaseIcon" },
-    { route: "education", name: "menu.education", icon: "AcademicCapIcon" },
-    { route: "project", name: "menu.project", icon: "CodeBracketIcon" },
+    { route: "/education", name: "menu.education", icon: "AcademicCapIcon" },
+    { route: "/project", name: "menu.project", icon: "CodeBracketIcon" },
   ];
 
   return (
@@ -25,20 +26,27 @@ const AboutCard = () => {
       </div>
       <div className="rounded-xl bg-white p-3 dark:bg-night-800">
         <ul className="inline-flex space-x-2 font-semibold">
-          {menu.map((item, index) => (
-            <li key={index}>
-              <Link
-                to={item.route}
-                className="inline-flex cursor-pointer items-center gap-1 rounded-lg border-2 border-gray-100 px-2 py-1.5 text-xs font-medium text-gray-400 hover:border-primary-500 hover:bg-primary-500 hover:text-white dark:border-night-700 dark:text-night-400 dark:hover:border-primary-500 dark:hover:bg-primary-500/20 dark:hover:text-primary-500"
-              >
-                <HeroIcon
-                  icon={item.icon}
-                  className="hidden h-4 w-4 sm:block"
-                />
-                <span>{i18n.t(item.name)}</span>
-              </Link>
-            </li>
-          ))}
+          {menu.map((item, index) => {
+            const isActive = location.pathname === item.route;
+            return (
+              <li key={index}>
+                <Link
+                  to={item.route}
+                  className={`inline-flex cursor-pointer items-center gap-1 rounded-lg border-2 px-2 py-1.5 text-xs font-medium ${
+                    isActive
+                      ? "bg-gray-100 text-gray-400 dark:bg-night-700 dark:text-night-300"
+                      : "border-gray-100 text-gray-400 hover:border-gray-100 hover:gray-100 hover:text-gray-400 dark:border-night-700 dark:text-night-400 dark:hover:border-primary-500 dark:hover:bg-primary-500/20 dark:hover:text-primary-500"
+                  }`}
+                >
+                  <HeroIcon
+                    icon={item.icon}
+                    className="hidden h-4 w-4 sm:block"
+                  />
+                  <span>{i18n.t(item.name)}</span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
