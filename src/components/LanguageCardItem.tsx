c4ba528d2da9649c3b0b95567179bdbebc3dc54d
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-24 12:38:11 +0630

    add language star indicator

--- src/components/LanguageCardItem.tsx
+++ src/components/LanguageCardItem.tsx
@@ -1,8 +1,32 @@
 import React from "react";
-import type { LanguageCardProps } from "../models/Language.ts";
 import { getImagePath } from "../helpers/Helper";
+import HeroIcon from "./HeroIcon.tsx";
 
-const LanguageCardItem: React.FC<LanguageCardProps> = ({ item }) => {
+const MAX_STARS = 5;
+
+const LanguageCardItem = ({ item }) => {
+  const level = Math.round(item.star);
+
+  const renderStars = () => {
+    return Array.from({ length: MAX_STARS }, (_, index) => {
+      return index < level ? (
+        // <SolidStarIcon key={index} className="h-4 w-4 text-yellow-500" />
+        <HeroIcon
+          icon="StarIcon"
+          isSolid={true}
+          className="h-4 w-4 text-yellow-500"
+        />
+      ) : (
+        // <OutlineStarIcon key={index} className="h-4 w-4 text-gray-300" />
+        <HeroIcon
+          icon="StarIcon"
+          isSolid={false}
+          className="h-4 w-4 text-gray-300"
+        />
+      );
+    });
+  };
+
   return (
     <div className="flex min-w-[180px] cursor-pointer items-center gap-2 rounded-lg border border-gray-100 px-4 py-2 hover:bg-gray-100 dark:border-night-700 dark:hover:bg-night-700">
       <img
@@ -18,11 +42,13 @@ const LanguageCardItem: React.FC<LanguageCardProps> = ({ item }) => {
           {item.level}
         </div>
 
-        <div className="w-full bg-gray-300 rounded-full h-2.5 dark:bg-night-700 mt-1">
-          <div
+        <div className="w-full bg-gray-300 rounded-full h-2.5 mb-1 dark:bg-night-700 mt-1">
+          <div className="flex items-center mt-1">{renderStars()}</div>
+
+          {/* <div
             className="bg-yellow-500 h-2.5 rounded-full"
             style={{ width: `${item.star * 20}%` }}
-          ></div>
+          ></div> */}
         </div>
       </div>
     </div>
